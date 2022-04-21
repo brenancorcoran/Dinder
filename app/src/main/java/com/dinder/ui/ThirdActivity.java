5434be57763e3c2e7a72559019799242452dfb33
hnichols <henryk.nichols@gmail.com>
2022-04-20 19:19:44 -0500

    - Button Edits

--- app/src/main/java/com/dinder/ui/ThirdActivity.java
+++ app/src/main/java/com/dinder/ui/ThirdActivity.java
@@ -2,9 +2,13 @@ package com.dinder.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.dinder.FifthActivity;
 import com.dinder.R;
+import com.dinder.SecondActivity;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -12,5 +16,13 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+    }
+
+    public void findLikeClick(View view) {
+        startActivity(new Intent(this, FifthActivity.class));
+    }
+
+    public void findDislikeClick(View view) {
+        startActivity(new Intent(this, SecondActivity.class));
     }
 }
